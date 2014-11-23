c4161c373df35c6c234e4cfe718d95697c806226
Julian Gindi <juliangindi@gmail.com>
2014-11-22 19:09:09 -0500

    Creating initial shell flow

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+)
+
+func main() {
+  fmt.Println("Welcome to GoShell")
+
+  // Creating a channel to communicate with the active shell session
+  messages := make(chan string)
+  go Reader(messages)
+
+  // Blocking until we get a message
+  <-messages
+}
+
+func Reader(messages chan string) {
+  reader := bufio.NewReader(os.Stdin)
+  for {
+    fmt.Print(">> ")
+    text, _ := reader.ReadString('\n')
+    fmt.Println(text)
+  }
+}
