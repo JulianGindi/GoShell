050d4051b20509ab4d9a3de5c8c946cd63be44a8
Julian Gindi <juliangindi@gmail.com>
2014-11-25 12:00:20 -0500

    Working 'cat' program

--- cat/cat.go
+++ cat/cat.go
@@ -1,18 +1,20 @@
-package cat
+package main
 
 import (
   "fmt"
   "io/ioutil"
+  "os"
 )
 
-func check(e error) {
+func check(e error, filename string) {
   if e != nil {
-    panic(e)
+    fmt.Println("No such file:", filename)
   }
 }
 
-func Cat(filename string) {
+func main() {
+  filename := os.Args[1]
   dat, err := ioutil.ReadFile(filename)
-  check(err)
+  check(err, filename)
   fmt.Print(string(dat))
 }
